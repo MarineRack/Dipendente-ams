79ba9e5f7efc6f161d57549fde298b2884566acf
IpSadico <gabrieleturiaco.gt@gmail.com>
2022-07-14 10:08:49 +0200

    spring inzlzr

---
+++ gestione_dipendenti_be/src/test/java/com/ams/gestione_dipendenti_be/GestioneDipendentiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ams.gestione_dipendenti_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneDipendentiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
