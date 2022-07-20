8f28b3dffee725867162dd3d159f10b289cbe6d5
MarineRack <69631305+MarineRack@users.noreply.github.com>
2022-07-20 11:11:04 +0200

    u

---
+++ gestione_dipendenti_be/src/main/gestione_dipendenti_be/repository/MesiRepository.java
@@ -0,0 +1,9 @@
+package com.ams.gestione_dipendenti_be.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ams.gestione_dipendenti_be.model.Mesi;
+
+public interface MesiRepository extends CrudRepository<Mesi, Integer> {
+
+}
